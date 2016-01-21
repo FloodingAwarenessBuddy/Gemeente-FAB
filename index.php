da999aa67389e0f6edac4f56e6571250f045f2a1
Niels <tnnielstn@gmail.com>
2016-01-21 22:07:44 +0100

    Location.js renamed to functions.js

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="scripts/init.js"></script>
     <script src="scripts/fab.js"></script>
-    <script src="scripts/Location.js"></script>
+    <script src="scripts/functions.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=<?=KEY?>&callback=initMap"></script>
     <script src="node_modules/mfb/src/mfb.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
